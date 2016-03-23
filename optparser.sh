36fef09cb4829d81483356969010f102db84120d
Kimiaki Kuno <knokmki612@gmail.com>
2016-03-23 17:19:25 +0900

    Clean: Refactor indentation

--- optparser.sh
+++ optparser.sh
@@ -1,48 +1,40 @@
 #!/bin/sh
 while [ $# -gt 0 ]; do
-	case $1 in
-		-*)
-			case $1 in
-				--*)
-					case $1 in
-						--hello-a)
-							echo 'hello a'
-							;;
-						--hello-b)
-							echo 'hello b'
-							;;
-						--*)
-							echo "invalid long option: $1"
-							exit 1
-					esac
-					shift 1
+	if echo $1 | grep -sq '\-\-.*'; then
+		case $1 in
+			--hello-a)
+				echo 'hello a'
+				;;
+			--hello-b)
+				echo 'hello b'
+				;;
+			--*)
+				echo "invalid long option: $1"
+				exit 1
+		esac
+		shift 1
+		continue
+	elif echo $1 | grep -sq '\-.*'; then
+		for shortopt in $(echo $1 | grep -o .); do
+			case $shortopt in
+				-)
 					continue
 					;;
+				a)
+					echo 'hello a'
+					;;
+				b)
+					echo 'hello b'
+					;;
 				*)
-					for shortopt in $(echo $1 | grep -o .); do
-						case $shortopt in
-							-)
-								continue
-								;;
-							a)
-								echo 'hello a'
-								;;
-							b)
-								echo 'hello b'
-								;;
-							*)
-								echo "invalid option: -$shortopt"
-								exit 1
-								;;
-						esac
-					done
+					echo "invalid option: -$shortopt"
+					exit 1
 					;;
 			esac
-			shift 1
-			continue
-			;;
-		*)
-			:
-			;;
-	esac
+		done
+		shift 1
+		continue
+	else
+		:
+	fi
 done
